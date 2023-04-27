2d27cf906dabc3fea4a83d48985942c58be27dda
SangeethaShanmugm <MY_NAME@example.com>
2023-04-27 12:09:30 +0530

    added endpoints

--- data.js
+++ data.js
@@ -1,10 +1,4 @@
-const express = require("express");
-const app = express();
-const PORT = 9000;
-// req => what you send/request to Server
-// res => what you receive from Server
-
-const products = [
+[
   {
     createdAt: "2023-04-03T13:54:19.653Z",
     name: "Rustic Metal Gloves",
@@ -14,7 +8,6 @@ const products = [
     unitsAvailable: 7,
     productImage: "https://loremflickr.com/640/480/food",
     id: "20",
-    category: "Tools",
   },
   {
     createdAt: "2023-04-03T14:41:02.813Z",
@@ -25,9 +18,25 @@ const products = [
     unitsAvailable: 19,
     productImage: "https://loremflickr.com/640/480/food",
     id: "21",
-    category: "Tech",
   },
-
+  {
+    createdAt: "2023-04-04T02:19:45.142Z",
+    name: "",
+    description: "",
+    price: 0,
+    unitsAvailable: 0,
+    productImage: "",
+    id: "22",
+  },
+  {
+    createdAt: "2023-04-04T01:18:37.281Z",
+    name: "",
+    description: "",
+    price: 0,
+    unitsAvailable: 0,
+    productImage: "",
+    id: "23",
+  },
   {
     createdAt: "2023-04-09T15:22:11.188Z",
     name: "Rustic Soft Computer",
@@ -37,7 +46,6 @@ const products = [
     unitsAvailable: 99,
     productImage: "https://loremflickr.com/640/480/food",
     id: "24",
-    category: "Tech",
   },
   {
     createdAt: "2023-04-09T18:00:29.779Z",
@@ -48,7 +56,6 @@ const products = [
     unitsAvailable: 6,
     productImage: "https://loremflickr.com/640/480/food",
     id: "25",
-    category: "Food",
   },
   {
     createdAt: "2023-04-09T17:02:48.754Z",
@@ -59,7 +66,6 @@ const products = [
     unitsAvailable: 20,
     productImage: "https://loremflickr.com/640/480/food",
     id: "26",
-    category: "Tools",
   },
   {
     createdAt: "2023-04-10T07:01:02.764Z",
@@ -70,7 +76,6 @@ const products = [
     unitsAvailable: 98,
     productImage: "https://loremflickr.com/640/480/food",
     id: "27",
-    category: "Food",
   },
   {
     createdAt: "2023-04-18T20:42:28.028Z",
@@ -81,35 +86,5 @@ const products = [
     unitsAvailable: 43,
     productImage: "https://loremflickr.com/640/480/food",
     id: "28",
-    category: "Tools",
   },
 ];
-
-//REST API endpoints
-app.get("/", (req, res) => {
-  res.send("Hello Everyone🥳🥳");
-});
-
-//get all products
-//Task
-// /products - get all products
-// /products?category=Tools - get only Tools product
-// /products?category=Tools&price=500 - filter by category and price
-// /products?price=500  - only products based on price
-
-app.get("/products", (req, res) => {
-  const { category } = req.query;
-  console.log(req.query);
-  const result = products.filter((pd) => pd.category == category);
-  res.send(result);
-});
-
-//get product by Id
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  const product = products.find((pd) => pd.id == id);
-  res.send(product);
-});
-
-app.listen(PORT, () => console.log("Server started on port ", PORT));
